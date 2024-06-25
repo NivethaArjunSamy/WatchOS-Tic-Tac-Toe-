a30f42a040513d8c5912703c97b4e16070f94d37
Nivetha ArjunSamy <nive2002k@gmail.com>
2024-06-25 12:05:21 +0530

    Initial Commit

---
+++ tictactoe WatchOs Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tictactoe WatchOs Watch App
+//
+//  Created by Nivetha ArjunSamy on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tictactoe WatchOs Watch App/tictactoe_WatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoe_WatchOsApp.swift
+//  tictactoe WatchOs Watch App
+//
+//  Created by Nivetha ArjunSamy on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct tictactoe_WatchOs_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
